a649c7daa5eca506ba2dc365cb3050c1d3f90ff0
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-10-10 20:47:40 -0500

    :clipboard: vutify added

--- src/store.js
+++ src/store.js
@@ -1,16 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    tickets:[],
+    user: null,
+    token: null,
+    loading: false,
+    error: null,
+    url: 'http://localhost:3000'
   },
   mutations: {
-
+    setLoadedTickets(state, payload){
+      state.tickets = payload
+    },
+    createTicket(state, payload){
+      state.tickets.push(payload)
+    },
+    setUser(state, payload){
+      state.user = payload
+    },
+    setToken(state, payload){
+      state.token = payload
+    },
+    setLoading(state, payload){
+      state.loading = payload
+    },
+    setError(state, payload){
+      state.error = payload
+    },
+    clearError(state, payload){
+      state.error = null
+    }
   },
   actions: {
+    loadTickets({commit, getters}){
+      axios.get(`${getters.url}/api/tickets`)
+      .then(response => {
+        console.log(response)
+        commit('setLoadedTickets', response.data.data)
+      })
+      .catch(err => {
 
+      })
+    },
+    createTicket({commit, getters}, payload){
+      axios.post(`${gettets.url}/api/tickets`, payload)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+
+      })
+    }
+  },
+  getters: {
+    loadedTickets(state){
+      return state.tickets
+    },
+    user(state){
+      return state.user
+    },
+    token(state){
+      return state.token
+    },
+    loading(state){
+      return state.loading
+    },
+    url(state){
+      return state.url
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        Tickets
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+      >
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.customer }}</td>
+          <td class="text-xs-right">{{ props.item.company }}</td>
+          <td class="text-xs-right">{{ props.item.subject }}</td>
+          <td class="text-xs-right">{{ props.item.description }}</td>
+          <td class="text-xs-right">{{ props.item.status.status }}</td>
+          <td class="text-xs-right">{{ props.item.assigned.name }}</td>
+          <td class="justify-center layout px-0">
+          <v-icon
+            small
+            class="mr-2 blue--text"
+            @click="editTicket(props.item)"
+          >
+            edit
+          </v-icon>
+          <v-icon
+            small
+            class="red--text"
+            @click="removeTicket(props.item)"
+          >
+            delete
+          </v-icon>
+        </td>
+        </template>
+        <v-alert slot="no-results" :value="true" color="error" icon="warning">
+          Your search for "{{ search }}" found no results.
+        </v-alert>
+      </v-data-table>
+    </v-card>
+
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style scoped>
 
+</style>
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      search: '',
+      loading: true,
+      headers: [
+          {
+            text: 'Customer',
+            align: 'left',
+            value: 'customer',
+          },
+          { text: 'Company', value: 'company' },
+          { text: 'Subject', value: 'subject' },
+          { text: 'Description', value: 'description' },
+          { text: 'Status', value: 'status' },
+          { text: 'Assigned to', value: 'assigned' },
+          {text: 'Actions', value: 'name', sortable: false}
+        ],
+        desserts: []
+      }
+  },
+  created(){
+    this.$store.dispatch('loadTickets')
+    this.desserts = this.tickets
+    
+  },
+  methods: {
+    editTicket(ticket){
+      console.log(ticket)
+    },
+    removeTicket(ticket){
+      console.log(ticket)
+    }
+  },
+  computed: {
+    tickets(){
+      return this.$store.getters.loadedTickets
+    }
   }
 }
 </script>
