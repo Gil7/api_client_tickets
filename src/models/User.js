cf66a84513f17f117c6aa4332511664bdbf95d62
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-10-13 23:30:06 -0500

    :clipboard: new model for users

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+export default class User{
+    constructor(id, name, lastname, email, role){
+        this.id = id
+        this.name = name
+        this.lastname = lastname
+        this.email = email
+        this.role = role
+    }
+}
