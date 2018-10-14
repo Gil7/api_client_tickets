20136e7d705792cc2ac57a376269eda5e0c71e5e
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-10-13 23:29:40 -0500

    :clipboard: about component created

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-layout>
+    <v-flex xs12 sm8 offset-sm2>
+      <v-card class="mt-4">
+        <v-img
+          src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
+          aspect-ratio="2.75"
+        ></v-img>
+
+        <v-card-title primary-title>
+          <div>
+            
+            <h3 class="headline mb-3">Application created by Gilberto Méndez</h3>
+            
+            <p>
+              <v-icon>
+                label_important
+              </v-icon>
+              Github: <a href="https://github.com/Gil7">Gil7</a>
+            </p>
+            <p>
+              <v-icon>
+                email
+              </v-icon>
+              Email: <a href="gilberto.mendez.santiz@gmaio.com">gilberto.mendez.santiz@gmail.com</a>
+            </p>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
+<style scope>
+
+</style>
